ece846220f3daee01dbda19873a1934066d09d9d
bblinder <bblinderman@hashicorp.com>
2021-07-22 19:27:20 +0000

    init

--- exercises/remote_backend.tf
+++ exercises/remote_backend.tf
@@ -5,9 +5,9 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "YOURORGANIZATION"
+    organization = "blindy-demos"
     workspaces {
-      name = "YOURWORKSPACE"
+      name = "hashicat-aws"
     }
   }
 }
--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Hiss World!</h2></center>
-  Welcome to ${PREFIX}'s app. The voices tell me to burn things.
+  Welcome to ${PREFIX}'s app. We take no responsibilities for allergies.
   <!-- END -->
 
   </div>
